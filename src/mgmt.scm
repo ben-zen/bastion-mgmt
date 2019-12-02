02d38dd21262656f4705dbb37daa540f084f52d4
Ben Lewis <1884340+ben-zen@users.noreply.github.com>
2019-12-01 16:15:45 -0800

    Added the basis of the show command.

--- src/mgmt.scm
+++ src/mgmt.scm
@@ -4,6 +4,8 @@
 ;; Copyright (C) 2019 Ben Lewis <zenrider@blacklodgeresearch.org>
 ;; License information found in the LICENSE file.
 
+(use-modules (ice-9 format))
+
 (define mgmt-connect-help
   '("mgmt-connect"
     "Connect to a device on the management network."
@@ -26,6 +28,22 @@
           (map (lambda (x) (display x) (newline)) mgmt-connect-help))
          (else (display "Unknown command."))))))
 
+(define (get-current-user)
+  "Get the current user's passwd entry as a vector."
+  (getpwuid (geteuid)))
+
+(define (get-current-user-name)
+  "Get the current user's username as seen in the loaded passwd entry."
+  (vector-ref (get-current-user) 0))
+
+(define (mgmt-show parameters)
+  "Lookup details about supported devices this user can access."
+  (if (nil? parameters)
+      (begin
+        (display (format #f "Status of devices managed by ~a:" (get-current-user-name)))
+        (newline))
+      ))
+
 (write (command-line))
 (newline)
 (let ((command (cadr (command-line))))
@@ -39,5 +57,4 @@
     (mgmt-help (cddr (command-line)))
     (newline))
    ((string=? command "show")
-    (display "Display information about the user's devices.")
-    (newline))))
+    (mgmt-show (cddr (command-line))))))
