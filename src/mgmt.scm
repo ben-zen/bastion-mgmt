75017fb3353a24b77d142e85a8b69e7f367b0b62
Ben Lewis <1884340+ben-zen@users.noreply.github.com>
2019-12-01 21:44:56 -0800

    Added a function skeleton to check if a machine can be used by a given user, as well as more documentation.

--- src/mgmt.scm
+++ src/mgmt.scm
@@ -10,22 +10,48 @@
   '("mgmt-connect"
     "Connect to a device on the management network."
     "Usage:"
-   "  mgmt connect device-name [port]"
-   "Parameters:"
-   "device-name: The name of a device as listed by the show command"
-   "port (Optional): The IP port with which to connect to the device,"
-   "                 if not the default."))
+    "  mgmt connect device-name [port]"
+    ""
+    "Parameters:"
+    "device-name: The name of a device as listed by the show command"
+    "port (Optional): The IP port with which to connect to the device,"
+    "                 if not the default."))
+
+(define mgmt-help-help
+  '("mgmt-help"
+    "Print this help message, or get help for an mgmt function."
+    "Usage:"
+    "  mgmt help [command]"
+    ""
+    "Parameters:"
+    "command (Optional): A command to get help with."
+    "  Supported commands: connect, show"))
+
+(define mgmt-show-help
+  '("mgmt-show"
+    "Get a summary of all your managed devices, or a detailed status of one."
+    "Usage:"
+    "  mgmt show [device-name]"
+    ""
+    "Parameters:"
+    "device-name (Optional): The device you want a detailed report on."))
+
+(define (print-help message)
+  "Print the provided message, line by line."
+  (map (lambda (x) (display x) (newline)) message))
 
 (define (mgmt-help parameters)
   "Print help for this tool, or a specific function thereof."
   (write parameters)
   (newline)
   (if (nil? parameters)
-      (display "Print this help message.")
+      (print-help mgmt-help-help)
       (let ((command (car parameters)))
         (cond
          ((string=? command "connect")
-          (map (lambda (x) (display x) (newline)) mgmt-connect-help))
+          (print-help mgmt-connect-help))
+         ((string=? command "show")
+          (print-help mgmt-show-help))
          (else (display "Unknown command."))))))
 
 (define (get-current-user)
@@ -35,6 +61,13 @@
 (define (get-current-user-name)
   "Get the current user's username as seen in the loaded passwd entry."
   (vector-ref (get-current-user) 0))
+
+(define (machine-allowed? machine-name user)
+  "Test if a machine is accesible by the supplied user."
+  ;; Initial design: lookup the machine in LDAP and check if the user is in the
+  ;; allowed users list. We should also check the user for groups that can be
+  ;; shared, such as overall network management.
+  #f)
 
 (define (mgmt-show parameters)
   "Lookup details about supported devices this user can access."
